e6cd7027a348743cff737416bc8065792b9f1f60
Abeer Rizik <abeer.rizik93@gmail.com>
2021-10-11 15:18:44 +0300

    adds home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Announcement from "../components/Announcement";
+import Navbar from "../components/Navbar";
+import Silder from "../components/Silder";
+
+const Home = () => {
+  return (
+    <div>
+      <Announcement />
+      <Navbar />
+      <Silder />
+    </div>
+  );
+};
+
+export default Home;
